e8553ef0cf5eda81cfc5a8d6d8b1f8e1dca21eea
guillem.cordoba <guillem.cordoba@gmail.com>
2024-09-29 17:10:18 +0200

    Chromium

--- home.nix
+++ home.nix
@@ -29,8 +29,8 @@
 
           nix eval -vvvvvvvvvvvvvvvvvvvv --raw --option trace-function-calls true $1 1>/dev/null 2> $WORKDIR/nix-function-calls.trace
           stack-collapse.py $WORKDIR/nix-function-calls.trace > $WORKDIR/nix-function-calls.folded
-          inferno-flamegraph $WORKDIR/nix-function-calls.folded > $WORKDIR/nix-function-calls.svg
-          chromium "$WORKDIR/nix-function-calls.svg"
+          inferno-flamegraph $WORKDIR/nix-function-calls.folded > $WORKDIR/nix-function-calls-$1.svg
+          chromium "$WORKDIR/nix-function-calls-$1.svg"
         '';
         checkPhase = "";
       };
